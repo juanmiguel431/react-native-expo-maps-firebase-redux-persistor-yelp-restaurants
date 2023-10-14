40be7c80c6b5a04a1b51752215c955fcabd72da2
juanmiguel431 <juanmiguel431@gmail.com>
2023-10-14 16:36:50 -0400

    Button added to the top right to access Setting screen.

--- App.tsx
+++ App.tsx
@@ -5,14 +5,32 @@ import { RootStackParamList, SCREEN } from './src/models/screen';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthScreen, DeckScreen, MapScreen, ReviewScreen, SettingScreen, WelcomeScreen } from './src/screens';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Button } from '@rneui/themed';
+import { View } from 'react-native';
 
 const Stack = createStackNavigator<RootStackParamList>();
 const Tab = createBottomTabNavigator<RootStackParamList>();
 
 const ReviewFlow = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name={SCREEN.Review} component={ReviewScreen}/>
+    <Stack.Navigator>
+      <Stack.Screen
+        name={SCREEN.Review}
+        component={ReviewScreen}
+        options={({ navigation }) => ({
+          headerRight: () => (
+            <View style={{ marginRight: 10 }}>
+              <Button
+                title="Setting"
+                size="sm"
+                onPress={() => {
+                  navigation.navigate(SCREEN.Setting);
+                }}
+              />
+            </View>
+          )
+        })}
+      />
       <Stack.Screen name={SCREEN.Setting} component={SettingScreen}/>
     </Stack.Navigator>
   );
@@ -23,7 +41,10 @@ const MainFlow = () => {
     <Tab.Navigator>
       <Tab.Screen name={SCREEN.Map} component={MapScreen}/>
       <Tab.Screen name={SCREEN.Deck} component={DeckScreen}/>
-      <Tab.Screen name={SCREEN.ReviewFlow} component={ReviewFlow}/>
+      <Tab.Screen
+        name={SCREEN.ReviewFlow}
+        component={ReviewFlow}
+        options={{ headerShown: false, title: 'Review Jobs' }}/>
     </Tab.Navigator>
   );
 };
