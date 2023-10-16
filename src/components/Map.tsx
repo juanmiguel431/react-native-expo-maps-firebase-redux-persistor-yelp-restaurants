c1220fc9e13f82cf990f4ee53cdca277544b708e
juanmiguel431 <juanmiguel431@gmail.com>
2023-10-15 21:22:04 -0400

    onRegionChangeComplete method added to Map Component

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,27 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleProp, StyleSheet, ViewStyle } from 'react-native';
-import MapView, { Polyline, Circle, PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { Polyline, Circle, PROVIDER_GOOGLE, Details, Region } from 'react-native-maps';
 import { Coordinate } from '../models/coordinate';
 
 type MapProps = {
-  initialRegion?: Coordinate;
+  initialRegion?: Region;
   currentLocation?: Coordinate;
   locations?: Coordinate[];
   height?: number;
   mapStyle?: StyleProp<ViewStyle>;
+  onRegionChangeComplete?: (region: Region, details: Details) => void
 }
 
-export const Map: React.FC<MapProps> = ({ currentLocation, locations, initialRegion, height, mapStyle }) => {
+export const Map: React.FC<MapProps> = (
+  { currentLocation, locations, initialRegion, height, mapStyle, onRegionChangeComplete }) => {
 
   return (
     <MapView
       provider={PROVIDER_GOOGLE}
       style={StyleSheet.flatten([styles.map, { height: height || 250 }, mapStyle])}
-      initialRegion={initialRegion ? {
-        ...initialRegion,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-      } : undefined}
+      initialRegion={initialRegion}
+      onRegionChangeComplete={onRegionChangeComplete}
     >
       {currentLocation &&
         <Circle
--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,12 +1,19 @@
-import React, { useCallback } from 'react';
-import { Text, View } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { MapScreenProps } from '../models/screen';
 import useLocation from '../hooks/useLocation';
 import { useIsFocused } from '@react-navigation/native';
 import { Point } from '../models/coordinate';
 import Map from '../components/Map';
+import { Details, Region } from 'react-native-maps';
 
 export const MapScreen: React.FC<MapScreenProps> = () => {
+  const [region, setRegion] = useState<Region>({
+    longitude: -122,
+    latitude: 37,
+    longitudeDelta: 0.04,
+    latitudeDelta: 0.09
+  });
 
   const isFocused = useIsFocused();
 
@@ -19,9 +26,26 @@ export const MapScreen: React.FC<MapScreenProps> = () => {
 
   const [errorMsg] = useLocation(shouldTrack, callBack);
 
+  const onRegionChangeComplete = useCallback((region: Region, details: Details) => {
+    setRegion(region);
+  }, []);
+
   return (
-    <View style={{ flex: 1 }}>
-      <Map mapStyle={{ flex: 1 }} />
+    <View style={styles.view}>
+      <Map
+        mapStyle={styles.map}
+        onRegionChangeComplete={onRegionChangeComplete}
+        initialRegion={region}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  },
+  view: {
+    flex: 1
+  }
+})
