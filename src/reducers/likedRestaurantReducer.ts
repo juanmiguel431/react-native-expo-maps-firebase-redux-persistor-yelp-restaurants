d089b7338365fe6ac6f6fa1a1e4bb6f45c615886
juanmiguel431 <juanmiguel431@gmail.com>
2023-10-18 21:48:13 -0400

    prevent duplicated liked restaurants using lodash

--- src/reducers/likedRestaurantReducer.ts
+++ src/reducers/likedRestaurantReducer.ts
@@ -1,5 +1,6 @@
 import Type from '../actions/types';
 import { Business } from '../models/yelp';
+import _ from 'lodash';
 
 type ReducerState = {
   items: Business[];
@@ -18,8 +19,10 @@ type ReducerAction = LikeRestaurantsAction;
 
 export const likedRestaurantReducer = (state: ReducerState = initialState, action: ReducerAction): ReducerState => {
   switch (action.type) {
-    case Type.LikeRestaurant:
-      return { ...state, items: [...state.items, action.payload] };
+    case Type.LikeRestaurant: {
+      const items = _.uniqBy([...state.items, action.payload], 'id');
+      return { ...state, items: items };
+    }
     default:
       return state;
   }
